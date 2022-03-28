4f564b12378e6d61c0b865565a2f431a5fc8578c
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-03-27 23:07:03 -0700

    CRUD database Connection

--- CRUD_DatabaseConnectivity/Form1.cs
+++ CRUD_DatabaseConnectivity/Form1.cs
@@ -7,14 +7,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
 
 namespace CRUD_DatabaseConnectivity
 {
     public partial class Form1 : Form
+
     {
+        SqlConnection connection;
+        SqlCommand command;
+        DataTable data = new DataTable();
+
+        string db_connection = @"Data Source=DESKTOP-T2H5N7O;initial Catalog=students;Integrated Security=true";
+
         public Form1()
         {
             InitializeComponent();
+            this.connection = new SqlConnection(db_connection);
+            data.Columns.Add("Name");
+            data.Columns.Add("Roll");
+            data.Columns.Add("CNIC");
+
+            dataGridView1.DataSource = data;
+            read_database();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void read_database()
+        {
+            if(!string.IsNullOrWhiteSpace(name.Text) && !string.IsNullOrWhiteSpace(roll.Text) && !string.IsNullOrWhiteSpace(cnic.Text))
+            {
+                try
+                {
+                    string sql = $"insert into student_table(Name,Roll,CNIC) " +
+                        $" values('{name.Text}','{roll.Text}','{cnic.Text}') ";
+                    connection.Open();
+                    command = new SqlCommand(sql, connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    adapter.InsertCommand = command;
+                    MessageBox.Show(adapter.InsertCommand.ExecuteNonQuery().ToString() + "New Record Added"
+                        );
+                    connection.Close();
+
+
+                }
+                catch(Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Something missing");
+            }
         }
     }
 }
